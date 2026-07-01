dbf9db2014aa92e07cbf3d7d1988a2b552b89f5d
vanhoa690 <vanhoa690@gmail.com>
2026-07-01 08:15:29 +0700

    update

--- learn-typescript/src/script.js
+++ learn-typescript/src/script.js
@@ -1,8 +1,1 @@
 console.log("javascript");
-console.log("hoadv");
-
-// let myName: string = "hoadv";
-// console.log(myName);
-
-let count = 10;
-count = "20";
--- learn-typescript/src/typescript.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log("typescript");
-let myName = "hoadv";
-console.log(myName);
-//# sourceMappingURL=typescript.js.map
